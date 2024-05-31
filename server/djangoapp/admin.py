59edd6a4c79aaeecc5ffbb24d3700f2f024e893f
mehmetkaynar <mehmetkynrr34@gmail.com>
2024-05-31 23:41:47 +0200

    CarMake and CarModel added

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,16 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
+
 
 # Register models here
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_user, name='logout'),
 
-    
+    # path get_cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
