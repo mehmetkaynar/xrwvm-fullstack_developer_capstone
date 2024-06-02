cddd6b6b38c44370e6e13c27a98e9c6d7914795f
mehmetkaynar <mehmetkynrr34@gmail.com>
2024-06-02 08:25:58 -0400

    modul 3 tamamlandi ama API de bir problem var

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('logout/', TemplateView.as_view(template_name="logout.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
